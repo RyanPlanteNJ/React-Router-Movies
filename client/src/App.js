10a47f54f95cb2c9d9b26abf416b7d1b8d381bf6
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-04-22 21:12:49 -0400

    starting on project

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link, Switch } from 'react-router-dom';
+
 
 import SavedList from './Movies/SavedList';
 
@@ -28,7 +30,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path="/movies/:id"></Route>
+        <Route path="/"></Route>
+      </Switch>
     </div>
   );
 };
